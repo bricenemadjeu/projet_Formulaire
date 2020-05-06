097a1fbc5e3d859d6bbfa660fc2e773d6a2a7403
Brice Nemadjeu <bricenemadjeu@gmail.com>
2020-05-06 17:16:49 +0100

    Update Login.java

--- src/main/java/serveletClass/Login.java
+++ src/main/java/serveletClass/Login.java
@@ -2,16 +2,15 @@ package serveletClass;
 
 import personne.User;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
-//import javax.servlet.annotation.WebServlet;
-
-@javax.servlet.annotation.WebServlet(name = "login")
+@WebServlet(name = "login")
 public class Login extends javax.servlet.http.HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
