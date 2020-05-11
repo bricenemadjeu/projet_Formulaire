6714b800c28254cd0e452f43b103d4b49b3b3967
Arletty <48282119+Arletty@users.noreply.github.com>
2020-05-11 17:18:27 +0100

    Create testclass.java

---
+++ src/main/java/personne/testclass.java
@@ -0,0 +1,36 @@
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testclass {
+  new User arlette =User ();
+    @BeforeAll
+    public static void setup() {
+      System.out.println("the class test is runing");
+    }
+
+    @BeforeEach
+    public void setupThis() {
+      System.out.println("this is the test runing");
+    }
+
+    @Test
+    public void method() {
+        org.junit.Assert.assertTrue(new ArrayList().isEmpty());
+
+    }
+
+    @AfterEach
+    public void tearThis() {
+      System.out.println("the test is complete");
+    }
+
+    @AfterAll
+    public static void tear() {
+      System.out.println("the test is complete");
+    }
+}
